22747c0aadc9bfcabd57798b048ec11b2fa176ea
Jakub Janik <jakubjanik00@gmail.com>
2021-04-08 05:42:33 +0200

    Improve characters fetching
    First fetch films and then change the list of film urls
    to list of film titles for each character.

--- src/App.tsx
+++ src/App.tsx
@@ -13,17 +13,63 @@ const GlobalStyle = createGlobalStyle`
 
 const Container = styled.main`
   display: flex;
+  flex-direction: column;
   justify-content: center;
 `;
 
+interface Film {
+  url: string;
+  title: string;
+};
+
 export default function App() {
+  const [films, setFilms] = useState<Film[]>([]);
   const [characters, setCharacters] = useState<Character[]>([]);
 
   useEffect(() => {
-    fetch('https://swapi.dev/api/people')
-      .then(response => response.json())
-      .then(({results}) => setCharacters(results));
+    async function fetch() {
+      if (!films.length) {
+        const response = await window.fetch('https://swapi.dev/api/films');
+
+        type JSONResponse = {
+          results: Film[];
+        };
+        const {results}: JSONResponse = await response.json();
+
+        const films = results.map(({url, title}) => ({url, title}))
+
+        setFilms(films);
+      }
+      
+      const response = await window.fetch('https://swapi.dev/api/people');
+
+      type JSONResponse = {
+        results: Character[];
+      };
+      const {results}: JSONResponse = await response.json();
+
+      const characters = results.map(character => {
+        const filmTitles = character.films.map(filmUrl => 
+          films.find(({url}) => url === filmUrl)?.title
+        )
+          
+        const {name, birth_year, gender, height} = character; 
+
+        return {
+          name,
+          birth_year,
+          gender,
+          height,
+          films: filmTitles as string[]
+        };
+      });
+
+      setCharacters(characters);
+    }
+
+    fetch();
   }, []);
+
 
   return (
     <Container>
