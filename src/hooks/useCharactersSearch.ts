70d253f00f1ac2b1b179f40df7e11c93ca537095
Jakub Janik <jakubjanik00@gmail.com>
2021-04-08 11:04:54 +0200

    Create useCharactersSearch hook

---
+++ src/hooks/useCharactersSearch.ts
@@ -0,0 +1,36 @@
+import {Dispatch, SetStateAction, useEffect, useState} from 'react';
+import {Character} from '../models/Character';
+
+type CharactersSearchResult = [
+  filteredCharacters: Character[],
+  queries: {
+    nameQuery: string;
+    filmQuery: string;
+  },
+  setQueries: {
+    setNameQuery: Dispatch<SetStateAction<string>>;
+    setFilmQuery: Dispatch<SetStateAction<string>>;
+  }
+];
+
+export function useCharactersSearch(characters: Character[]): CharactersSearchResult {
+  const [filteredCharacters, setFilteredCharacters] = useState<Character[]>(characters) 
+  const [nameQuery, setNameQuery] = useState('');
+  const [filmQuery, setFilmQuery] = useState('');
+
+  useEffect(() => {
+    if (!nameQuery && !filmQuery) {
+      setFilteredCharacters(characters);
+      return;
+    }
+
+    setFilteredCharacters(
+      characters.filter(character => 
+        character.name.includes(nameQuery) && 
+        character.films.join(',').includes(filmQuery)
+      )
+    );
+  }, [nameQuery, filmQuery, characters]);
+
+  return [filteredCharacters, {nameQuery, filmQuery}, {setNameQuery, setFilmQuery}];
+};
